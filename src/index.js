4acd00764a30a20af509275fb534f868a02ae9ee
nkolchakov <nikola.kolchakov@gmail.com>
2019-04-14 15:23:06 +0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 
 // console.log(process.env.MY_SECRET);
-
+const port = process.env.PORT;
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () =>
-    console.log('Example app listening on port 3000!'),
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port} !`),
 );
