7f09de90cac7e19cb496e59be3eff629f872bb40
nkolchakov <nikola.kolchakov@gmail.com>
2019-04-15 21:12:00 +0300

    send and recieve basi data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,69 @@
 import 'dotenv/config';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import uuidv4 from 'uuid/v4';
 
-// console.log(process.env.MY_SECRET);
 const port = process.env.PORT;
 const app = express();
+
+// Parses the text as JSON and exposes the resulting object on req.body.
+app.use(bodyParser.json());
+
+// Parses the text as URL encoded data 
+// (which is how browsers tend to send form data from regular forms set to POST) 
+// and exposes the resulting object (containing the keys and values) on req.body.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+};
+  
+const messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'By World',
+        userId: '2',
+    },
+};
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/users', (req, res) => {
+    return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+});
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+      id,
+      text: req.body.text
+    };
+  
+    messages[id] = message;
+    return res.send(message);
+});
+
 
 app.listen(port, () =>
     console.log(`Example app listening on port ${port} !`),
