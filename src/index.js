97875af80d989bf18aba46900537b40ceea8b84a
nkolchakov <nikola.kolchakov@gmail.com>
2019-04-27 21:43:50 +0300

    refactor models into seperate file and add context to request object.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import express from 'express';
 import uuidv4 from 'uuid/v4';
 
+import models from './models/index';
+
 const port = process.env.PORT;
 const app = express();
 
@@ -16,33 +18,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // simple version of a middleware that determines a pseudo 
 // “authenticated” user that is sending the request
 app.use((req, res, next) => {
-    req.me = users[1];
+    req.context = {
+        models,
+        me: models.users[1]
+    }
     next();
 });
-
-const users = {
-    1: {
-      id: '1',
-      username: 'Robin Wieruch',
-    },
-    2: {
-      id: '2',
-      username: 'Dave Davids',
-    },
-};
-  
-const messages = {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'By World',
-        userId: '2',
-    },
-};
 
 app.use(cors());
 
@@ -50,25 +31,49 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
-});
-
-app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
-});
-
-app.post('/messages', (req, res) => {
+app.get('/session', (req, res) => {
+    return res.send(req.context.models.users[req.context.me.id]);
+  });
+  
+  app.get('/users', (req, res) => {
+    return res.send(Object.values(req.context.models.users));
+  });
+  
+  app.get('/users/:userId', (req, res) => {
+    return res.send(req.context.models.users[req.params.userId]);
+  });
+  
+  app.get('/messages', (req, res) => {
+    return res.send(Object.values(req.context.models.messages));
+  });
+  
+  app.get('/messages/:messageId', (req, res) => {
+    return res.send(req.context.models.messages[req.params.messageId]);
+  });
+  
+  app.post('/messages', (req, res) => {
     const id = uuidv4();
     const message = {
       id,
-      text: req.body.text,     
-      userId: req.me.id,
+      text: req.body.text,
+      userId: req.context.me.id,
     };
   
-    messages[id] = message;
+    req.context.models.messages[id] = message;
+  
     return res.send(message);
-});
+  });
+  
+  app.delete('/messages/:messageId', (req, res) => {
+    const {
+      [req.params.messageId]: message,
+      ...otherMessages
+    } = req.context.models.messages;
+  
+    req.context.models.messages = otherMessages;
+  
+    return res.send(message);
+  });
 
 
 app.listen(port, () =>
