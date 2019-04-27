d10f00d9174aa31230311b8dcd42560f72c42f10
nkolchakov <nikola.kolchakov@gmail.com>
2019-04-27 21:52:30 +0300

    refactor routes into seperate modules

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
-import uuidv4 from 'uuid/v4';
+import routes from './routes';
 
 import models from './models/index';
 
@@ -30,50 +30,10 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
-app.get('/session', (req, res) => {
-    return res.send(req.context.models.users[req.context.me.id]);
-  });
-  
-  app.get('/users', (req, res) => {
-    return res.send(Object.values(req.context.models.users));
-  });
-  
-  app.get('/users/:userId', (req, res) => {
-    return res.send(req.context.models.users[req.params.userId]);
-  });
-  
-  app.get('/messages', (req, res) => {
-    return res.send(Object.values(req.context.models.messages));
-  });
-  
-  app.get('/messages/:messageId', (req, res) => {
-    return res.send(req.context.models.messages[req.params.messageId]);
-  });
-  
-  app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-      id,
-      text: req.body.text,
-      userId: req.context.me.id,
-    };
-  
-    req.context.models.messages[id] = message;
-  
-    return res.send(message);
-  });
-  
-  app.delete('/messages/:messageId', (req, res) => {
-    const {
-      [req.params.messageId]: message,
-      ...otherMessages
-    } = req.context.models.messages;
-  
-    req.context.models.messages = otherMessages;
-  
-    return res.send(message);
-  });
 
 
 app.listen(port, () =>
