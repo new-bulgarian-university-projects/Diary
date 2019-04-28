2c5b4e1b7a8908e972475705348c9095bef2af80
nkolchakov <nikola.kolchakov@gmail.com>
2019-04-28 17:10:01 +0300

    refresh db data on startup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
@@ -34,8 +35,53 @@ app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
+const eraseDatabaseOnSync = true;
+
 connectDb().then(async () => {
-    app.listen(process.env.PORT, () =>
-      console.log(`Example app listening on port ${process.env.PORT}!`),
-    );
-  });
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({}),
+    ]);
+
+    createUsersWithMessages();
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
+});
+
+const createUsersWithMessages = async () => {
+    const user1 = new models.User({
+      username: 'rwieruch',
+    });
+  
+    const user2 = new models.User({
+      username: 'ddavids',
+    });
+  
+    const message1 = new models.Message({
+      text: 'Published the Road to learn React',
+      user: user1.id,
+    });
+  
+    const message2 = new models.Message({
+      text: 'Happy to release ...',
+      user: user2.id,
+    });
+  
+    const message3 = new models.Message({
+      text: 'Published a complete ...',
+      user: user2.id,
+    });
+  
+    await message1.save();
+    await message2.save();
+    await message3.save();
+  
+    await user1.save();
+    await user2.save();
+
+    console.log("seeded.")
+};
