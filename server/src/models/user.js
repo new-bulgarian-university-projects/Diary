3d695753b1327080d53637f68551d52b20a1e989
nkolchakov <nikola.kolchakov@gmail.com>
2019-04-30 19:58:33 +0300

    remove unused stuff stuff

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -17,11 +17,6 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// //  pre hook to our user schema to remove all messages of this user on its deletion
-// userSchema.pre('remove', function(next) {
-//     this.model('Message').deleteMany({ user: this._id }, next);
-//   });
-
 const User = mongoose.model('User', userSchema);
 
 export default User;
